d9687384051bb90fed67019ad312c4d2431c54b3
Diego Garcia <diego.garcia@sandbox.game>
2024-04-20 07:10:44 -0300

    uploading app

---
+++ vite-gif/src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${ import.meta.env.VITE_GIPHY_API_KEY }&q=${ category }&limit=10`;
+    console.log(url);
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+    
+    return gifs;
+}
